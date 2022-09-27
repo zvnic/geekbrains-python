9c94690942f3df1f9d35d27c9ecc8197e96553c3
zvnic <zvnman@yandex.ru>
2022-09-27 10:03:30 +0300

    dz-2 all done

--- dz-2/dz_2_task_4.py
+++ dz-2/dz_2_task_4.py
@@ -3,6 +3,22 @@
 Позиции вводятся с клавиатуры.
 """
 
-n = int(input("Введите число N: "))
+from random import randint
 
-print(sequenceСalculation(n))
+def initArray(n):
+    initArr = [2 * n]
+    for i in range(2*n):
+        initArr.append(randint(-n, n))
+    return initArr
+
+n = int(input('Введите размер массива: '))
+array = initArray(n)
+print(f'Вывод массиса: {array}')
+
+index_a = int(input('Введите номер индекса первого элемнта : '))
+index_b = int(input('Введиет номер индекса второго элемента: '))
+
+if 0 < index_a > len(array)-1 or 0 < index_b > len(array)-1:
+    print('Ошибка номера элемента массива!')
+else:
+    print(f'Mult of elements: {array[index_a]} * {array[index_b]} =', array[index_a] * array[index_b])
