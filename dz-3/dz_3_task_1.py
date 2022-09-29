10be86f7c5d0cf0b6c939ac087e68217d40a85d0
zvnic <zvnman@yandex.ru>
2022-09-29 21:12:34 +0300

    dz-3 task-1

--- dz-3/dz_3_task_1.py
+++ dz-3/dz_3_task_1.py
@@ -3,3 +3,22 @@
 Пример:
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 """
+
+import random
+
+def getListRandomValue(size):
+    arr = random.sample(range(1, size+1), size)
+    return arr
+
+def sumOddElements(arr):
+    sum = 0
+    for i in range(len(arr)):
+        if(i % 2 != 0):
+            print(f"arr[{i}]={arr[i]}")
+            sum += arr[i]
+    return sum
+
+arr = getListRandomValue(5)
+print(arr)
+
+print(f"Сумма нечётных элементов = {sumOddElements(arr)}")
