959432b73459d6e145651912159f8ad9660c909c
zvnic <zvnman@yandex.ru>
2022-10-10 22:04:30 +0300

    dz-5 done

--- dz-5/task_1.py
+++ dz-5/task_1.py
@@ -1,12 +1,55 @@
 """
-Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+Создайте программу для игры с конфетами человек против человека.
+Условие задачи:
+На столе лежит 2021 конфета.
+Играют два игрока делая ход друг после друга.
+Первый ход определяется жеребьёвкой.
+За один ход можно забрать не более чем 28 конфет.
+Все конфеты оппонента достаются сделавшему последний ход.
+Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+a) Добавьте игру против бота
 """
 
-def findAndReplaceText(str, word):
-    for char in word:
-        str = str.replace(char, "")
-    return str.replace("  ", " ")
+from random import randint
 
-str = input("Введите исходный текст: ")
+def stepPlayer(name):
+    k = int(input(f"{name}, возьмите конфеты от 1 до 28 штук: "))
+    while k < 1 or k > 28:
+        k = int(input(f"{name}, введите количество от 1 до 28 штук: "))
+    return k
 
-print(f"Из текста удалены слова содержащие \"абв\": \n {findAndReplaceText(str, 'абв')}")
+def stepResult(name, k, counter, value):
+    print(f"{name}, взял {k} шт., теперь у него всего {counter} шт. Осталось на столе {value} конфет.")
+
+player1 = input("Введите Ваше Имя: ")
+player2 = 'Компьютер'
+value = int(input("Введите начальное количество конфет на столе: "))
+
+counter1 = 0
+counter2 = 0
+
+active_player = randint(0,2)
+
+if active_player:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+while value > 28:
+    if active_player:
+        k = stepPlayer(player1)
+        counter1 += k
+        value -= k
+        active_player = False
+        stepResult(player1, k, counter1, value)
+    else:
+        k = randint(1,29)
+        counter2 += k
+        value -= k
+        active_player = True
+        stepResult(player2, k, counter2, value)
+
+if active_player:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл Компьютер")
--- dz-5/task_2.py
+++ dz-5/task_2.py
@@ -1,54 +1,61 @@
 """
-Создайте программу для игры с конфетами человек против человека.
-Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой.
-За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход.
-
-Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
-a) Добавьте игру против бота
-b) Подумайте как наделить бота ""интеллектом""
+Создайте программу для игры в ""Крестики-нолики"".
 """
+import os
 
-from random import randint
+# карта с индексами
+maps = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def stepPlayer(name):
-    k = int(input(f"{name}, возьмите конфеты от 1 до 28 штук: "))
-    while k < 1 or k > 28:
-        k = int(input(f"{name}, введите количество от 1 до 28 штук: "))
-    return k
+# победные линийи
+winLine = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+
+# Вывод карты
+def print_maps():
+    print(f"{maps[0]} {maps[1]} {maps[2]}")
+    print(f"{maps[3]} {maps[4]} {maps[5]}")
+    print(f"{maps[6]} {maps[7]} {maps[8]}")
+
+# ход в клетку
+def stepPlayer(step, symbol):
+    ind = maps.index(step)
+    maps[ind] = symbol
+
+# определяем рузультат
+def getWinLine():
+    win = ""
+
+    for i in winLine:
+        if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+            win = "X"
+        if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+            win = "O"
+
+    return win
 
 
-def stepResult(name, k, counter, value):
-    print(f"{name}, взял {k} шт., теперь у него всего {counter} шт. Осталось на столе {value} конфет.")
+game_over = False
+player = True
 
-player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
-value = int(input("Введите начальное количество конфет на столе: "))
+while game_over == False:
 
-counter1 = 0
-counter2 = 0
+    print_maps()
 
-flag = randint(0,2)
-
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
-
-while value > 28:
-    if flag:
-        k = stepPlayer(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        stepResult(player1, k, counter1, value)
+    if player == True:
+        symbol = "X"
+        step = int(input("Игрок Х, ваш ход, укажите номер клетки: "))
     else:
-        k = stepPlayer(player2)
-        counter2 += k
-        value -= k
-        flag = True
-        stepResult(player2, k, counter2, value)
+        symbol = "O"
+        step = int(input("Игрок О, ваш ход, укажите номер клетки: "))
 
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+    stepPlayer(step, symbol)
+    win = getWinLine()
+
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
+
+    player = not player
+
+print_maps()
+print("Победил", win)
